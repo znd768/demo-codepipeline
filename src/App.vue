48e5485d84ebf1e512d35d87f8644d2d24992cbf
znd768 <rasksk37@gmail.com>
2025-12-18 00:59:18 +0900

    commit-1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Nothing works!" />
+      <HelloWorld msg="Nothing works2!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
